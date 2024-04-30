aee3158267bf826505faa4a3263721a41a476a17
Petros Devrikis <100411628+Petrosdevri@users.noreply.github.com>
2024-04-30 18:47:17 +0000

    Fixed functionality in TS

--- public/js/script.js
+++ public/js/script.js
@@ -1,36 +1,43 @@
 "use strict";
 const fromCurrency = document.getElementById("from-currency");
-const fromAmount = document.getElementById("from-amount");
+const fromAmountElement = document.getElementById("from-amount");
 const toCurrency = document.getElementById("to-currency");
 const toAmount = document.getElementById("to-amount");
 const rate = document.getElementById("rate");
 const exchange = document.getElementById("exchange");
-if (fromCurrency)
-    fromCurrency.addEventListener('change', convert);
-if (fromAmount)
-    fromAmount.addEventListener('input', convert);
-if (toCurrency)
-    toCurrency.addEventListener('change', convert);
-if (toAmount)
-    toAmount.addEventListener('input', convert);
-if (exchange && fromCurrency && fromAmount && toCurrency && toAmount)
-    exchange.addEventListener('click', () => {
-        let tempCurrency = fromCurrency.value;
-        let tempAmount = fromAmount.value;
-        fromCurrency.value = toCurrency.value;
-        fromAmount.value = toAmount.value;
-        toCurrency.value = tempCurrency;
-        toAmount.value = tempAmount;
-    });
-function convert() {
-    if (fromCurrency && typeof fromAmount === 'number' && toCurrency && toAmount && rate)
-        fetch(`https://api.exchangerate-api.com/v4/latest/${fromCurrency.value}`)
-            .then(res => res.json())
-            .then(res => {
-            const rateValue = res.rates[`${toCurrency.value}`];
-            rate.innerHTML = `1 ${fromCurrency.value} = ${rateValue ? rateValue : ' '} ${toCurrency.value}`;
-            toAmount.value = (rateValue * fromAmount.value).toFixed(2);
-        })
-            .catch(error => console.log('Error:', error));
+function isHTMLInputElement(element) {
+    return element !== null && element instanceof HTMLInputElement;
 }
-convert();
+if (isHTMLInputElement(fromAmountElement)) {
+    const fromAmount = fromAmountElement;
+    if (fromCurrency)
+        fromCurrency.addEventListener('change', convert);
+    if (fromAmount)
+        fromAmount.addEventListener('input', convert);
+    if (exchange && fromCurrency && fromAmount && toCurrency && toAmount)
+        exchange.addEventListener('click', () => {
+            let tempCurrency = fromCurrency.value;
+            let tempAmount = fromAmount.value;
+            fromCurrency.value = toCurrency.value;
+            fromAmount.value = toAmount.value;
+            toCurrency.value = tempCurrency;
+            toAmount.value = tempAmount;
+        });
+    if (toCurrency)
+        toCurrency.addEventListener('change', convert);
+    if (toAmount)
+        toAmount.addEventListener('input', convert);
+    function convert() {
+        if (fromCurrency && toCurrency && toAmount && rate) {
+            fetch(`https://api.exchangerate-api.com/v4/latest/${fromCurrency.value}`)
+                .then(res => res.json())
+                .then(res => {
+                const rateValue = res.rates[`${toCurrency.value}`];
+                rate.innerHTML = `1 ${fromCurrency.value} = ${rateValue ? rateValue : ' '} ${toCurrency.value}`;
+                toAmount.value = (rateValue * parseInt(fromAmount.value)).toFixed(2);
+            })
+                .catch(error => console.log('Error:', error));
+        }
+    }
+    convert();
+}
--- public/ts/script.ts
+++ public/ts/script.ts
@@ -1,33 +1,42 @@
-const fromCurrency: HTMLSelectElement | null = document.getElementById("from-currency");
-const fromAmount: HTMLInputElement | null = document.getElementById("from-amount");
-const toCurrency: HTMLSelectElement | null = document.getElementById("to-currency");
-const toAmount: HTMLInputElement | null = document.getElementById("to-amount");
-const rate: HTMLDivElement | null = document.getElementById("rate");
-const exchange = document.getElementById("exchange");
+const fromCurrency = document.getElementById("from-currency") as HTMLSelectElement;
+const fromAmountElement = document.getElementById("from-amount");
+const toCurrency = document.getElementById("to-currency") as HTMLSelectElement;
+const toAmount = document.getElementById("to-amount") as HTMLInputElement;
+const rate = document.getElementById("rate") as HTMLElement;
+const exchange = document.getElementById("exchange") as HTMLButtonElement;
 
-if (fromCurrency) fromCurrency.addEventListener('change', convert);
-if (fromAmount) fromAmount.addEventListener('input', convert);
-if (toCurrency) toCurrency.addEventListener('change', convert);
-if (toAmount) toAmount.addEventListener('input', convert);
-if (exchange && fromCurrency && fromAmount && toCurrency && toAmount) exchange.addEventListener('click', () => {
-    let tempCurrency = fromCurrency.value;
-    let tempAmount = fromAmount.value;
-    fromCurrency.value = toCurrency.value;
-    fromAmount.value = toAmount.value;
-    toCurrency.value = tempCurrency;
-    toAmount.value = tempAmount;
-});
-
-function convert() {
-    if (fromCurrency && typeof fromAmount === 'number' && toCurrency && toAmount && rate)
-    fetch(`https://api.exchangerate-api.com/v4/latest/${fromCurrency.value}`)
-        .then(res => res.json())
-        .then(res => {
-            const rateValue = res.rates[`${toCurrency.value}`];
-            rate.innerHTML = `1 ${fromCurrency.value} = ${rateValue ? rateValue : ' '} ${toCurrency.value}`;
-            toAmount.value = (rateValue * fromAmount.value).toFixed(2);
-        })
-        .catch(error => console.log('Error:', error));
+function isHTMLInputElement(element: HTMLElement | null): element is HTMLInputElement {
+    return element !== null && element instanceof HTMLInputElement;
 }
 
-convert();
+if (isHTMLInputElement(fromAmountElement)) {
+    const fromAmount = fromAmountElement;
+
+    if (fromCurrency) fromCurrency.addEventListener('change', convert);
+    if (fromAmount) fromAmount.addEventListener('input', convert);
+    if (exchange && fromCurrency && fromAmount && toCurrency && toAmount) exchange.addEventListener('click', () => {
+        let tempCurrency = fromCurrency.value;
+        let tempAmount = fromAmount.value;
+        fromCurrency.value = toCurrency.value;
+        fromAmount.value = toAmount.value;
+        toCurrency.value = tempCurrency;
+        toAmount.value = tempAmount;
+    });
+    if (toCurrency) toCurrency.addEventListener('change', convert);
+    if (toAmount) toAmount.addEventListener('input', convert);
+
+    function convert() {
+        if (fromCurrency && toCurrency && toAmount && rate) {
+            fetch(`https://api.exchangerate-api.com/v4/latest/${fromCurrency.value}`)
+                .then(res => res.json())
+                .then(res => {
+                    const rateValue: number = res.rates[`${toCurrency.value}`];
+                    rate.innerHTML = `1 ${fromCurrency.value} = ${rateValue ? rateValue : ' '} ${toCurrency.value}`;
+                    toAmount.value = (rateValue * parseInt(fromAmount.value)).toFixed(2);
+                })
+                .catch(error => console.log('Error:', error));
+        }
+    }
+    
+    convert();
+}
